4403a32a2bae9ef35911f494472c5d925c76d200
Shibil Muhammad <shibilmuhammad678@gmail.com>
2024-03-12 14:05:37 +0530

    server initialized

---
+++ server/App.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const userrouter = require('./routes/userRouter')
+const adminrouter = require('./routes/adminRouter')
+const mongoose = require("mongoose")
+const session = require('express-session');
+const cookieParser = require("cookie-parser");
+
+app.use(cors())
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(session({
+    secret: 'your-secret-key', // Change this to a secure random string
+    resave: false,
+    saveUninitialized: true,
+  }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/codelab')
+.then(() => {
+    console.log("database connected");
+})
+.catch(() =>{
+    console.log("error on connecting db")
+})
+
+
+app.use("/",userrouter)
+app.use("/admin",adminrouter)
+
+app.listen(4000,()=>{
+    console.log("started");
+})
